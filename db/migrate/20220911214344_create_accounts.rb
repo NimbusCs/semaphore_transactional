9e7fc017a3df70d1eb9046def142cb3248bc2e00
Tomas Nuñez <IxidorCs@gmail.com>
2022-09-11 16:49:06 -0500

    account model with CRUD

---
+++ db/migrate/20220911214344_create_accounts.rb
@@ -0,0 +1,13 @@
+class CreateAccounts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :accounts do |t|
+      t.bigint :user_id, null: false
+      t.string :name, default: 'Principal'
+      t.integer :type_a, default: 0
+      t.boolean :enabled, default: true
+      t.text :meta_data
+
+      t.timestamps
+    end
+  end
+end
