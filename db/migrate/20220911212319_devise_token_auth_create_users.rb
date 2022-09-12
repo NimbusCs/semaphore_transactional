ba9940bac5e93ecba06ff1672f975b745aad68f4
Tomas Nuñez <IxidorCs@gmail.com>
2022-09-12 03:00:33 -0500

    fix db testing

--- db/migrate/20220911212319_devise_token_auth_create_users.rb
+++ db/migrate/20220911212319_devise_token_auth_create_users.rb
@@ -41,7 +41,6 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
