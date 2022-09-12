5a4693a8bfdae88cf87b1467c1477d49338e8877
Tomas Nuñez <IxidorCs@gmail.com>
2022-09-11 21:45:42 -0500

    status on text

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,14 @@ class Transaction < ApplicationRecord
     before_create :create_tx_id
     belongs_to :user
 
+    def status_text
+        [[0,'Pending'],[1,'Completed'],[2,'Canceled']].find { |k,v| k == self.status }.last
+    end
+
+    def as_json(options={:except => []})
+        super(options.merge!(methods: [:status_text]))
+    end
+
     protected
 
     def create_tx_id
